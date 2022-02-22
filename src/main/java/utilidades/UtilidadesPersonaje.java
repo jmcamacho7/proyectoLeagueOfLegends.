901ed2912c3ab4fda047be04875bdc06c742b998
jmcamacho7 <camachosanchezjm@gmail.com>
2022-02-22 20:47:10 +0100

    Primeros metodos realizados

---
+++ src/main/java/utilidades/UtilidadesPersonaje.java
@@ -0,0 +1,103 @@
+package utilidades;
+
+import modelos.Personaje;
+import modelos.Region;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UtilidadesPersonaje {
+
+    public Personaje levelUp(Personaje personaje){
+        if (personaje.getNivel()<18){
+            personaje.setNivel(personaje.getNivel()+1);
+            personaje.setAtaqueBase(personaje.getAtaqueBase() + personaje.getEscalabilidad().getIncrementoDanioNivel() * personaje.getNivel());
+            personaje.setDefensaBase(personaje.getDefensaBase() + personaje.getEscalabilidad().getIncrementoDefensaNivel() * personaje.getNivel());
+            personaje.setManaBase(personaje.getManaBase() + personaje.getEscalabilidad().getIncrementoManaNivel() * personaje.getNivel());
+            personaje.setVidaBase(personaje.getVidaBase() + personaje.getEscalabilidad().getIncrementoSaludNivel() * personaje.getNivel());
+        }
+
+        return personaje;
+    }
+
+    public Map<Region, List<Personaje>> getPersonajesPorRegion(List<Personaje> personajes){
+        Map<Region, List<Personaje>> lista = new HashMap<>();
+
+        List<Personaje> Demaciano = new ArrayList<>();
+        List<Personaje> Noxiano = new ArrayList<>();
+        List<Personaje> Jonio = new ArrayList<>();
+        List<Personaje> Shurimanos = new ArrayList<>();
+        List<Personaje> Targoniano = new ArrayList<>();
+        List<Personaje> Freijordiano = new ArrayList<>();
+        List<Personaje> Piltillo = new ArrayList<>();
+        List<Personaje> Zaunita = new ArrayList<>();
+        List<Personaje> Bandle = new ArrayList<>();
+        List<Personaje> AguasEstancadas = new ArrayList<>();
+        List<Personaje> Desconocido = new ArrayList<>();
+
+        for (Personaje x : personajes){
+            if (x.getRegion2() == Region.DEMACIA){
+                Demaciano.add(x);
+            }
+            if (x.getRegion2() == Region.NOXUS){
+                Noxiano.add(x);
+            }
+            if (x.getRegion2() == Region.JONIA){
+                Jonio.add(x);
+            }
+            if (x.getRegion2() == Region.SHURIMA){
+                Shurimanos.add(x);
+            }
+            if (x.getRegion2() == Region.TARGON){
+                Targoniano.add(x);
+            }
+            if (x.getRegion2() == Region.FREIJORD){
+                Freijordiano.add(x);
+            }
+            if (x.getRegion2() == Region.PILTOVER){
+                Piltillo.add(x);
+            }
+            if (x.getRegion2() == Region.ZAUM){
+                Zaunita.add(x);
+            }
+            if (x.getRegion2() == Region.BANDLE){
+                Bandle.add(x);
+            }
+            if (x.getRegion2() == Region.AGUAS_ESTANCADAS){
+                AguasEstancadas.add(x);
+            }
+            if (x.getRegion2() == Region.DESCONOCIDA){
+                Desconocido.add(x);
+            }
+        }
+
+        lista.put(Region.DEMACIA, Demaciano);
+        lista.put(Region.NOXUS, Noxiano);
+        lista.put(Region.JONIA, Jonio);
+        lista.put(Region.SHURIMA, Shurimanos);
+        lista.put(Region.TARGON, Targoniano);
+        lista.put(Region.FREIJORD, Freijordiano);
+        lista.put(Region.PILTOVER, Piltillo);
+        lista.put(Region.ZAUM, Zaunita);
+        lista.put(Region.BANDLE, Bandle);
+        lista.put(Region.AGUAS_ESTANCADAS,AguasEstancadas);
+        lista.put(Region.DESCONOCIDA, Desconocido);
+
+        return lista;
+
+    }
+
+    public Personaje getMasPoderoso(List<Personaje> personajes){
+        List<Personaje> campeones = new ArrayList<>();
+
+        for (Personaje x : personajes){
+
+            campeones.add(x);
+        }
+
+    }
+
+
+}
