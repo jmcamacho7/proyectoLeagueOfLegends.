a241e626cff9b585b2c35b33233ff61ee5638c48
jmcamacho7 <camachosanchezjm@gmail.com>
2022-03-13 20:47:26 +0100

    trabajo acabado (sin revisión)

--- src/main/java/utilidades/UtilidadesPartida.java
+++ src/main/java/utilidades/UtilidadesPartida.java
@@ -1,4 +1,52 @@
 package utilidades;
 
+import modelos.Jugador;
+import modelos.Partida;
+import modelos.Personaje;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class UtilidadesPartida {
+
+    private void inicializarPartida(Partida partida, List<Jugador> participantes, List<Personaje> personajesDisponibles){
+        Map<Jugador, Personaje> elecciones = new HashMap<>();
+        List<Jugador> primerequipo = new ArrayList<>();
+        for (Jugador x: participantes){
+
+            for (Personaje y: personajesDisponibles){
+
+                if (x.getPersonajeFavorito().isEmpty() == false){
+                    Personaje favorito = new Personaje();
+
+                    for (Personaje z : personajesDisponibles){
+                        if (z == x.getPersonajeFavorito()){
+                            favorito = z;
+                        }
+                    }
+                    elecciones.put(x, favorito);
+                    partida.setElecciones(elecciones);
+                }
+
+                if (x.getPersonajeFavorito().isEmpty() == true){
+                    Personaje azar = new Personaje();
+                    Collections.shuffle(personajesDisponibles);
+                    azar = personajesDisponibles.get(1);
+                    elecciones.put(x, y);
+                    partida.setElecciones(elecciones);
+                }
+            }
+
+            Jugador participante = new Jugador();
+            participante = x;
+        }
+        partida.setInicioPartida(LocalDateTime.now());
+    }
+
+    private void finalizarPartida(Partida partida, Integer equipoVencedor){
+        partida.setFinPartida(LocalDateTime.now());
+        partida.setEquipoVencedor(equipoVencedor);
+
+    }
 }
