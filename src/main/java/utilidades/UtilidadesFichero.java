aea307a562baaa8b2bd243af4cc3c70986e41c74
jmcamacho7 <camachosanchezjm@gmail.com>
2022-03-13 15:10:08 +0100

    Primer metodo v6 acabado

--- src/main/java/utilidades/UtilidadesFichero.java
+++ src/main/java/utilidades/UtilidadesFichero.java
@@ -4,11 +4,63 @@ import modelos.Personaje;
 
 import java.util.List;
 
-public class UtilidadesFichero {
-    public List<Personaje> leerPersonajes(){
-        public static final String SEPARATOR=";";
-        public static final String QUOTE="\\";
+import com.opencsv.CSVReader;
+import modelos.Region;
 
+import java.awt.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UtilidadesFichero {
+
+    public UtilidadesFichero() {
     }
 
+    public static final char SEPARATOR=',';
+    public static final char QUOTE='"';
+
+    public static List<Personaje>  leerPersonajes() throws IOException {
+
+        List<Personaje> campeon = new ArrayList<>();
+        CSVReader reader = null;
+
+        try {
+            reader = new CSVReader(new FileReader("C:\\Users\\hakue\\IdeaProjects\\proyectoLeagueOfLegends\\src\\main\\java\\documentos"),SEPARATOR,QUOTE);
+            String[] nextLine= null ;
+            int count = 0;
+
+            while ((nextLine = reader.readNext()) != null) {
+
+                if(count >0) {
+                    String[] valores = nextLine;
+                    Personaje x = new Personaje();
+                    x.setId(Integer.parseInt(valores[0]));
+                    x.setNombre(valores[1]);
+                    x.setFechaCreacion(LocalDate.parse(valores[2], DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+                    x.setAtaqueBase(Double.valueOf(valores[3]));
+                    x.setDefensaBase(Double.valueOf(valores[4]));
+                    x.setVidaBase(Double.valueOf(valores[5]));
+                    x.setManaBase(Double.valueOf(valores[6]));
+                    x.setRegion2(Region.valueOf(valores[7]));
+                    campeon.add(x);
+                }
+                count++;
+            }
+
+        } catch (Exception e) {
+            throw e;
+        }
+
+        reader.close();
+
+        return campeon;
+    }
+
+
 }
+
